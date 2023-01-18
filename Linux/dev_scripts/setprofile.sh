200c5deba7ab174158999feb14f5da34ac73ac35
Kristo Copani <31226503+kristocopani@users.noreply.github.com>
2023-01-19 00:41:10 +0200

    Update setprofile.sh

--- Linux/dev_scripts/setprofile.sh
+++ Linux/dev_scripts/setprofile.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Download the functions file
-curl -LJO https://raw.githubusercontent.com/USERNAME/REPO/BRANCH/.bashrc
+curl -LJO https://raw.githubusercontent.com/kristocopani/dotfiles/main/Linux/dev_scripts/bash_myfunctions.txt
 
 # Move the downloaded file to the home directory
-mv .bash_myfunctions ~/.bash_myfunctions
+mv bash_myfunctions.txt ~/.bash_myfunctions
 
 # Append the contents of the aliases file to the .bashrc file
-if [ -f ~/.bash_myfunctions ]; then
-    cat ./bashrc >> ~/.bashrc
-fi
+#if [ -f ~/.bash_myfunctions ]; then
+#    cat ./bashrc >> ~/.bashrc
+#fi
 
 # Source the .bashrc file
-source ~/.bashrc
+#source ~/.bashrc
